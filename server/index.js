b71a138baec7bc78140822b171510642df7bf5d9
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2022-05-28 17:50:52 +0200

    Scheletro base della Web App con MERN

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const port = 8080;
+const app = express();
+const DB_URL = "mongodb://localhost/test";
+
+mongoose.connect(DB_URL, { useNewUrlParser: true})
+    .then(() => {
+        console.log("Successfully connected to the database")
+        
+        // Una volta connessi al database, è possibile mettere il server in ascolto
+        app.listen(port, () => console.log(`Server listening on port ${port}`))
+    })
+    .catch((error) => console.log(error.message));
+
+app.get('/', (req, res) => {
+    res.status(200).send("<h3>Server working</h3>");
+});
