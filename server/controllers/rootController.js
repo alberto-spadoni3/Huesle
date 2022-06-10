b944add768f5dfd78bebf496efa9738145c6a9c8
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2022-06-10 18:49:02 +0200

    Sostituito il server http con quuello fornito da express

--- server/controllers/rootController.js
+++ server/controllers/rootController.js
@@ -1,12 +1,9 @@
-import dbManager from "./dbManager.js";
-
 const index = (req, res) => {
     res.status(200).send("<h2>Server working</h2>");
- }
+};
 
- const rootHandlers = {
+const rootHandlers = {
     index,
-    searchMatch
 };
 
 export default rootHandlers;
--- server/routes/rootRoute.js
+++ server/routes/rootRoute.js
@@ -1,13 +1,7 @@
-import express from 'express';
-import rootHandlers from '../controllers/rootController.js';
+import express from "express";
+import rootHandlers from "../controllers/rootController.js";
 const router = express.Router();
 
-router.route('/')
-    .get(rootHandlers.index);
-router.route('/register')
-    .get(rootHandlers.register);
-router.route('/login')
-    .post(rootHandlers.login);
-
+router.route("/").get(rootHandlers.index);
 
 export default router;
