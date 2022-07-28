ae48a56d132775f7a62d006428f38b6c81c4d214
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2022-07-28 16:55:38 +0200

    Modifiche varie

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -1,65 +1,81 @@
 import {
     createRandomSolutionWithRepetition,
     createSolutionWithoutRepetition,
-    elaborateTurn,
-    GameStates, isMatchOver, isPlayerTurn
+    GameStates,
 } from "../model/gameLogic.js";
 
 import mongoose from "mongoose";
 
-import {PendingRequestModel} from "../model/PendingRequestModel.js";
-import {UserModel} from "../model/UserModel.js";
-import {MatchModel} from "../model/MatchModel.js";
-import {emitNewMatch, emitNewMove, emitMatchOver} from "../middlewares/socketHandler.js"
+import { PendingRequestModel } from "../model/PendingRequestModel.js";
+import { UserModel } from "../model/UserModel.js";
+import { MatchModel } from "../model/MatchModel.js";
+import {
+    emitNewMatch,
+    emitNewMove,
+    emitMatchOver,
+} from "../middlewares/socketHandler.js";
 
 async function findUserId(username) {
-    const account = await UserModel.findOne({'username':username}, '_id')
-    if(!account) return;
+    const account = await UserModel.findOne({ username: username }, "_id");
+    if (!account) return;
     else return account._id.toString();
 }
 
 const searchMatch = async (req, res) => {
-    const {username, secret} = req.body;
+    const username = req.username;
+    const { secret } = req.body;
     const requesterId = await findUserId(username);
-    if(!requesterId) return res.status(400).json({
-        message: "Username not valid"
-    });
+    if (!requesterId)
+        return res.status(400).json({
+            message: "Username not valid",
+        });
 
     let pendingRequest;
-    if(secret) {
-        pendingRequest = await PendingRequestModel
-            .where("playerId").equals(requesterId)
-            .where("secretCode").ne(null).findOne();
+    if (secret) {
+        pendingRequest = await PendingRequestModel.where("playerId")
+            .equals(requesterId)
+            .where("secretCode")
+            .ne(null)
+            .findOne();
     } else {
-        pendingRequest = await PendingRequestModel
-            .where("playerId").equals(requesterId)
-            .where("secretCode").equals(null).findOne();
+        pendingRequest = await PendingRequestModel.where("playerId")
+            .equals(requesterId)
+            .where("secretCode")
+            .equals(null)
+            .findOne();
     }
-    console.log(pendingRequest);
-    if(pendingRequest) {
+
+    if (pendingRequest) {
         return res.status(400).json({
-            message: "User already pending for match with those specifics"
+            message: "User already pending for match with those specifics",
         });
     }
 
-    if(!secret) {
+    if (!secret) {
         //Finding public match
-        const pendingRequest = await PendingRequestModel
-            .where("secretCode").equals(null)
-            .where("playerId").ne(requesterId).findOne();
+        const pendingRequest = await PendingRequestModel.where("secretCode")
+            .equals(null)
+            .where("playerId")
+            .ne(requesterId)
+            .findOne();
+
         if (pendingRequest) {
             pendingRequest.deleteOne();
-            const newMatch = await createMatch(requesterId, pendingRequest.playerId, true);
+            const newMatch = await createMatch(
+                requesterId,
+                pendingRequest.playerId,
+                true
+            );
             return res.status(200).json({
-                matchId: newMatch._id
+                matchId: newMatch._id,
             });
         }
     }
 
     //Create new request
     let pendingRequestToSave = {
-        playerId: requesterId
-    }
+        playerId: requesterId,
+    };
     if (secret) {
         pendingRequestToSave.secretCode = await generateSecretCode();
     }
@@ -67,59 +83,66 @@ const searchMatch = async (req, res) => {
     await newPendingRequest.save();
     return res.status(200).json({
         message: "Searching other contestant",
-        secretCode: pendingRequestToSave.secretCode
+        secretCode: pendingRequestToSave.secretCode,
     });
-}
+};
 
 const joinPrivateMatch = async (req, res) => {
-    const {username, secretCode} = req.body;
+    const { username, secretCode } = req.body;
     const requesterId = await findUserId(username);
-    if(!requesterId) return res.status(400).json({
-        message: "Username not valid"
-    });
+    if (!requesterId)
+        return res.status(400).json({
+            message: "Username not valid",
+        });
 
-    let pendingRequest = await PendingRequestModel
-        .where("playerId").ne(requesterId)
-        .where("secretCode").equals(secretCode)
+    let pendingRequest = await PendingRequestModel.where("playerId")
+        .ne(requesterId)
+        .where("secretCode")
+        .equals(secretCode)
         .findOne();
 
-    if(pendingRequest) {
+    if (pendingRequest) {
         pendingRequest.deleteOne();
-        const newMatch = await createMatch(requesterId, pendingRequest.playerId, true);
+        const newMatch = await createMatch(
+            requesterId,
+            pendingRequest.playerId,
+            true
+        );
         return res.status(200).json({
-            matchId: newMatch._id
+            matchId: newMatch._id,
         });
     } else {
         return res.status(404).json({
-            message: "No match found with that secret code"
+            message: "No match found with that secret code",
         });
     }
-}
+};
 
 const leaveSearchPrivateMatch = async (req, res) => {
-    const {username} = req.body;
+    const { username } = req.body;
 
     const requesterId = await findUserId(username);
-    if(!requesterId) return res.status(400).json({
-            error: "Username not valid"
+    if (!requesterId)
+        return res.status(400).json({
+            error: "Username not valid",
         });
 
-    const pendingRequest = await PendingRequestModel
-        .where("playerId").equals(requesterId)
+    const pendingRequest = await PendingRequestModel.where("playerId")
+        .equals(requesterId)
         //.where("secretCode").ne(null)
         .findOne();
 
-    if(pendingRequest) {
+    if (pendingRequest) {
         pendingRequest.deleteOne();
         return res.status(200).json({
-            message: "Search abandoned successfully"
+            message: "Search abandoned successfully",
         });
     } else {
         return res.status(400).json({
-            error: "No pending request found"
+            error: "No pending request found",
         });
     }
-}
+};
 
 function createMatch(p1, p2, repetitions) {
     const matchDoc = {
@@ -127,12 +150,12 @@ function createMatch(p1, p2, repetitions) {
         status: {
             state: GameStates.PLAYING,
             turn: 0,
-            player: p1
+            player: p1,
         },
-        date: mongoose.now()
-    }
+        date: mongoose.now(),
+    };
 
-    if(repetitions) {
+    if (repetitions) {
         matchDoc.solution = createRandomSolutionWithRepetition();
     } else {
         matchDoc.solution = createSolutionWithoutRepetition();
@@ -147,9 +170,11 @@ async function generateSecretCode() {
     let duplicateCode, secretCode;
     do {
         secretCode = Math.floor(Math.random() * 99999);
-        duplicateCode = await PendingRequestModel.where("secretCode").equals(secretCode).findOne();
+        duplicateCode = await PendingRequestModel.where("secretCode")
+            .equals(secretCode)
+            .findOne();
     } while (duplicateCode);
-    while(secretCode.toString().length < 5) secretCode = "0" + secretCode;
+    while (secretCode.toString().length < 5) secretCode = "0" + secretCode;
     return secretCode;
 }
 
