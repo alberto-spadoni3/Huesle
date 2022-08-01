41af624bcc55f7345e02c088cfd3340643d139ca
Alberto Spadoni <alberto.spadoni3@studio.unibo.it>
2022-08-01 12:12:31 +0200

    Aggiunti headers di default per i metodi PUT e GET

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -7,9 +7,11 @@ export default axios.create({
 
 const axiosPrivate = axios.create({
     baseURL: BASE_URL,
-    withCredentials: true
+    withCredentials: true,
 });
 
+axiosPrivate.defaults.headers.get["Content-Type"] = "application/json";
+axiosPrivate.defaults.headers.put["Content-Type"] = "application/json";
 axiosPrivate.defaults.headers.post["Content-Type"] = "application/json";
 
-export {axiosPrivate};
+export { axiosPrivate };
